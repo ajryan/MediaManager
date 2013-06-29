9058ed34617c11115b60a546c49dbf6ccb3320d1
ajryan <ryan.aidan@gmail.com>
2013-06-29 01:03:58 -0700

    tfssteno dump all parts

--- MediaManager.Web/Controllers/TfsStenoController.cs
+++ MediaManager.Web/Controllers/TfsStenoController.cs
@@ -13,29 +13,36 @@ namespace MediaManager.Web.Controllers
         {
             var requestMulti = await request.Content.ReadAsMultipartAsync();
 
-            string to = await requestMulti.Contents[2].ReadAsStringAsync();
-            string subject = await requestMulti.Contents[8].ReadAsStringAsync();
-            string body = await requestMulti.Contents[3].ReadAsStringAsync();
-
-            Trace.TraceInformation(String.Format("Received message. To: {0}; Subject: {1}, Body: {2}", to, subject, body));
-
-            int workItemId;
-            if (!Int32.TryParse(to.Substring(to.IndexOf('@') + 1), out workItemId))
+            for (int contentIndex = 0; contentIndex < requestMulti.Contents.Count; contentIndex++)
             {
-                string failMessage = "Could not parse workitem ID from " + to;
-                Trace.TraceInformation(failMessage);
-                return new HttpResponseMessage(HttpStatusCode.OK)
-                {
-                    Content = new StringContent(failMessage)
-                };
+                var part = requestMulti.Contents[contentIndex];
+                Trace.TraceInformation("Part {0}: {1}", contentIndex, await part.ReadAsStringAsync());
             }
 
-            Trace.TraceInformation("WorkItem ID: " + workItemId);
+            //string to = await requestMulti.Contents[2].ReadAsStringAsync();
+            //string subject = await requestMulti.Contents[8].ReadAsStringAsync();
+            //string body = await requestMulti.Contents[3].ReadAsStringAsync();
+
+            //Trace.TraceInformation(String.Format("Received message. To: {0}; Subject: {1}, Body: {2}", to, subject, body));
+
+            //int workItemId;
+            //if (!Int32.TryParse(to.Substring(to.IndexOf('@') + 1), out workItemId))
+            //{
+            //    string failMessage = "Could not parse workitem ID from " + to;
+            //    Trace.TraceInformation(failMessage);
+            //    return new HttpResponseMessage(HttpStatusCode.OK)
+            //    {
+            //        Content = new StringContent(failMessage)
+            //    };
+            //}
+
+            //Trace.TraceInformation("WorkItem ID: " + workItemId);
             return new HttpResponseMessage(HttpStatusCode.OK)
             {
-                Content = new StringContent("Appended to history of work item " + workItemId)
+                //Content = new StringContent("Appended to history of work item " + workItemId)
+                Content = new StringContent("temp")
             };
-            
+
         }
     }
 }
