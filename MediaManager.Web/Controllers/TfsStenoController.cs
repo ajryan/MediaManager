a21477be1c322ea8aee5c8c97759c6583400cc3f
ajryan <ryan.aidan@gmail.com>
2013-06-28 22:54:36 -0700

    tfssteno JSON body

--- MediaManager.Web/Controllers/TfsStenoController.cs
+++ MediaManager.Web/Controllers/TfsStenoController.cs
@@ -5,15 +5,30 @@ using System.Web.Http;
 
 namespace MediaManager.Web.Controllers
 {
+    public class Email
+    {
+        public string[] to;
+        public string from;
+        public string subject;
+        public string text;
+        public string html;
+        public int attachments;
+
+        public override string ToString()
+        {
+            return String.Format(
+                "to: {0}; subject: {1}, html: {2}, text: {3}",
+                to, subject, html, text);
+        }
+    }
+
     public class TfsStenoController : ApiController
     {
-        public HttpResponseMessage Post(string to, string subject, string html, string text)
+        public HttpResponseMessage Post([FromBody] Email email)
         {
             return new HttpResponseMessage(HttpStatusCode.OK)
             {
-                Content = new StringContent(String.Format(
-                    "Got request with to: {0}; subject: {1}, html: {2}, text: {3}",
-                    to, subject, html, text))
+                Content = new StringContent(email.ToString())
             };
         }
     }
